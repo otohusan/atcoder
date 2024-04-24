ce318c699cda16bbb3547f6a4a5d739a8a2fdf67
otohusan <tamioguitar@gmail.com>
2024-04-24 13:39:30 +0900

    できた

--- abc/330/c.cpp
+++ abc/330/c.cpp
@@ -5,4 +5,20 @@ using ll = long long int;
 int main() {
     ll D;
     cin >> D;
+
+    ll ans = 100000000001;
+    double rd = sqrt(D);
+
+    for (ll i = 0; i < rd; i++) {
+
+        if (i * i - D >= 0) {
+            ans = min(ans, i * i - D);
+        } else {
+            ll y = sqrt(D - i * i);
+            ans = min(ans, abs(i * i + y * y - D));
+            ans = min(ans, abs(i * i + (y + 1) * (y + 1) - D));
+        }
+    }
+
+    cout << ans;
 }
