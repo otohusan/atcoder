8d5414ebbdd9440d0203def66c4b689e7c489a8b
otohusan <tamioguitar@gmail.com>
2024-05-03 18:12:20 +0900

    320b

--- abc/320/b.cpp
+++ abc/320/b.cpp
@@ -5,4 +5,23 @@ using ll = long long int;
 vector<int> dy = {1, 0, -1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    string S;
+    cin >> S;
+
+    ll ans = 0;
+
+    for (int i = 0; i < S.size() - 1; i++) {
+        for (int j = 0; j < S.size() - i + 1; j++) {
+            string s = S.substr(i, j);
+            string s_r = s;
+            reverse(s_r.begin(), s_r.end());
+
+            if (s == s_r) {
+                ans = max(ans, (ll)s.length());
+            }
+        }
+    }
+
+    cout << ans;
+}
