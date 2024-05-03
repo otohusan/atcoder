d0670e01d9c03011b98170ac0f61ed6e781fb8c5
otohusan <tamioguitar@gmail.com>
2024-05-03 21:44:25 +0900

    321c

--- abc/321/c.cpp
+++ abc/321/c.cpp
@@ -5,4 +5,24 @@ using ll = long long int;
 vector<int> dy = {1, 0, -1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll K;
+    cin >> K;
+
+    vector<ll> ans;
+
+    for (ll bit = 2; bit < (1 << 10); bit++) {
+        ll x = 0;
+        for (ll j = 9; j >= 0; j--) {
+            if (bit & (1 << j)) {
+                x *= 10;
+                x += j;
+            }
+        }
+        ans.push_back(x);
+    }
+
+    sort(ans.begin(), ans.end());
+
+    cout << ans[K - 1];
+}
