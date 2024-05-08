5f3b154a7c9f2a2d4fc333af2733ce219ddd33a8
otohusan <tamioguitar@gmail.com>
2024-05-08 16:35:38 +0900

    318c

--- abc/318/c.cpp
+++ abc/318/c.cpp
@@ -5,4 +5,29 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, D, P;
+    cin >> N >> D >> P;
+    vector<ll> F(N);
+    for (auto &f : F)
+        cin >> f;
+    sort(F.begin(), F.end());
+
+    ll ans = 0;
+
+    ll count = 0;
+    ll f_sum = 0;
+    for (ll i = N - 1; i >= 0; i--) {
+        f_sum += F[i];
+        count++;
+
+        if (count % D == 0) {
+            ans += min(f_sum, P);
+            f_sum = 0;
+        }
+    }
+
+    ans += min(f_sum, P);
+
+    cout << ans;
+}
