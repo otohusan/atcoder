a053a791cc1f02912af8b90537072465dc7624eb
otohusan <tamioguitar@gmail.com>
2024-05-08 12:31:40 +0900

    318a

--- abc/319/b.cpp
+++ abc/319/b.cpp
@@ -2,22 +2,18 @@
 using namespace std;
 using ll = long long int;
 // U, R, D, L
-vector<int> dy = {-1, -1, 0, 1, 1, 1, 0, -1};
-vector<int> dx = {0, 1, 1, 1, 0, -1, -1, -1};
+vector<int> dy = {1, 0, -1, 0};
+vector<int> dx = {0, 1, 0, -1};
 
 int main() {
-    int n;
-    cin >> n;
+    ll N, M, P;
+    cin >> N >> M >> P;
 
-    string s;
-    for (int i = 0; i <= n; i++) {
-        s += "-";
-        for (int j = 1; j <= 9; j++) {
-            if (n % j == 0 && i % (n / j) == 0) {
-                s[i] = j + '0';
-                break;
-            }
-        }
+    ll ans = 0;
+
+    for (int i = M; i < N + 1; i += P) {
+        ans++;
     }
-    cout << s << endl;
+
+    cout << ans;
 }
