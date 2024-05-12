463944f7309c1df5cb378c4f1e8639d0ef9476d7
otohusan <tamioguitar@gmail.com>
2024-05-12 15:46:44 +0900

    317c

--- abc/317/c.cpp
+++ abc/317/c.cpp
@@ -1,8 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long int;
-// U, R, D, L
-vector<int> dy = {-1, 0, 1, 0};
-vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int N, M;
+vector<pair<int, int>> adj[11]; // 隣接リスト（ノード番号と道路の長さ）
+
+void dfs(int current, vector<bool> &visited, ll pathLength, ll &maxLength) {
+    bool isEnd = true;
+    for (auto &edge : adj[current]) {
+        int next = edge.first;
+        int length = edge.second;
+        if (!visited[next]) {
+            visited[next] = true;
+            isEnd = false;
+            dfs(next, visited, pathLength + length, maxLength);
+            visited[next] = false;
+        }
+    }
+    if (isEnd) { // これ以上訪れる場所がない場合
+        maxLength = max(maxLength, pathLength);
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    for (int i = 0; i < M; i++) {
+        int A, B, C;
+        cin >> A >> B >> C;
+        adj[A].push_back({B, C});
+        adj[B].push_back({A, C});
+    }
+
+    ll maxLength = 0;
+    for (int i = 1; i <= N; i++) {
+        vector<bool> visited(N + 1, false);
+        visited[i] = true;
+        dfs(i, visited, 0, maxLength); // 各ノードから探索を開始
+    }
+
+    cout << maxLength << endl;
+    return 0;
+}
