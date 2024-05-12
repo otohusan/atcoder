4a44693f2be392a276944a828acac28fff6e50f1
otohusan <tamioguitar@gmail.com>
2024-05-12 15:53:55 +0900

    315a

--- abc/315/a.cpp
+++ abc/315/a.cpp
@@ -5,4 +5,26 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    string S;
+    cin >> S;
+
+    string ans = "";
+
+    for (int i = 0; i < S.size(); i++) {
+        if (S[i] == 'a')
+            continue;
+        if (S[i] == 'i')
+            continue;
+        if (S[i] == 'e')
+            continue;
+        if (S[i] == 'o')
+            continue;
+        if (S[i] == 'u')
+            continue;
+
+        ans += S[i];
+    }
+
+    cout << ans;
+}
