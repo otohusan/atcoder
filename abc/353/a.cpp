26fabf05ca89b92412c9c94e27deb466dd2d4ebe
otohusan <tamioguitar@gmail.com>
2024-05-15 19:40:50 +0900

    353a

--- abc/353/a.cpp
+++ abc/353/a.cpp
@@ -5,4 +5,20 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> H(N);
+    for (auto &h : H)
+        cin >> h;
+    ll k = H[0];
+
+    for (int i = 1; i < N; i++) {
+        if (k < H[i]) {
+            cout << i + 1;
+            return 0;
+        }
+    }
+
+    cout << -1;
+}
