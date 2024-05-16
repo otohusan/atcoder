3db997311c586c7d1f7b7b8c1461dce81d0eacba
otohusan <tamioguitar@gmail.com>
2024-05-16 10:59:00 +0900

    313c

--- abc/313/c.cpp
+++ abc/313/c.cpp
@@ -5,4 +5,29 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> A(N);
+    ll sum = 0;
+    for (auto &a : A) {
+        cin >> a;
+        sum += a;
+    }
+
+    vector<ll> B(N, sum / N);
+
+    sort(A.begin(), A.end());
+
+    for (int i = 0; i < sum % N; i++) {
+        B[N - 1 - i]++;
+    }
+
+    ll ans = 0;
+
+    for (int i = 0; i < N; i++) {
+        ans += abs(A[i] - B[i]);
+    }
+
+    cout << ans / 2;
+}
