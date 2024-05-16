b762fc100513969a87716726a08299bb40eb7bb2
otohusan <tamioguitar@gmail.com>
2024-05-16 18:01:01 +0900

    312c間違いパターン

--- abc/312/c.cpp
+++ abc/312/c.cpp
@@ -5,4 +5,26 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, M;
+    cin >> N >> M;
+    vector<ll> A(N), B(M);
+    for (auto &a : A)
+        cin >> a;
+    for (auto &a : B)
+        cin >> a;
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+
+    for (ll i = 0; i < N; i++) {
+        auto it = lower_bound(B.begin(), B.end(), A[i]);
+
+        if (it != B.end() && M - (it - B.begin()) <= i + 1) {
+            cout << A[i];
+            return 0;
+        }
+    }
+
+    cout << B[M - 1] + 1;
+}
