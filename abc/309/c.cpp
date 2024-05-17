344c2715e1ec3a07eafc8b64010a72966fe64f2b
otohusan <tamioguitar@gmail.com>
2024-05-17 16:51:03 +0900

    309c

--- abc/309/c.cpp
+++ abc/309/c.cpp
@@ -5,4 +5,30 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, K;
+    cin >> N >> K;
+    ll sum = 0;
+    map<ll, ll> M;
+
+    for (ll i = 0; i < N; i++) {
+        ll a, b;
+        cin >> a >> b;
+        M[a] += b;
+        sum += b;
+    }
+
+    if (sum <= K) {
+        cout << 1;
+        return 0;
+    }
+
+    for (auto m : M) {
+        sum -= m.second;
+
+        if (sum <= K) {
+            cout << m.first + 1;
+            return 0;
+        }
+    }
+}
