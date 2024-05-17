0c35a1ab48ab4363867a37e4ee81f4c9a19a74d6
otohusan <tamioguitar@gmail.com>
2024-05-17 09:52:09 +0900

    312c

--- abc/312/c.cpp
+++ abc/312/c.cpp
@@ -8,23 +8,19 @@ vector<int> dx = {0, 1, 0, -1};
 int main() {
     ll N, M;
     cin >> N >> M;
-    vector<ll> A(N), B(M);
-    for (auto &a : A)
+    vector<ll> A;
+    for (int i = 0; i < N; i++) {
+        ll a;
         cin >> a;
-    for (auto &a : B)
+        A.push_back(a);
+    }
+    for (int i = 0; i < M; i++) {
+        ll a;
         cin >> a;
-
-    sort(A.begin(), A.end());
-    sort(B.begin(), B.end());
-
-    for (ll i = 0; i < N; i++) {
-        auto it = lower_bound(B.begin(), B.end(), A[i]);
-
-        if (it != B.end() && M - (it - B.begin()) <= i + 1) {
-            cout << A[i];
-            return 0;
-        }
+        A.push_back(a + 1);
     }
 
-    cout << B[M - 1] + 1;
+    sort(A.begin(), A.end());
+
+    cout << A[M - 1];
 }
