baaede7700179ebd3273d8331cdf97c55dab37f6
otohusan <tamioguitar@gmail.com>
2024-05-17 16:06:26 +0900

    310c

--- abc/310/c.cpp
+++ abc/310/c.cpp
@@ -5,4 +5,25 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N;
+    cin >> N;
+
+    set<string> Store;
+    ll ans = 0;
+
+    for (ll i = 0; i < N; i++) {
+        string S;
+        cin >> S;
+
+        if (Store.count(S))
+            continue;
+
+        Store.insert(S);
+        reverse(S.begin(), S.end());
+        Store.insert(S);
+        ans++;
+    }
+
+    cout << ans;
+}
