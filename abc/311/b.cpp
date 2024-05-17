ad37de099d989a5af91fc7ed3d730928beb29a1e
otohusan <tamioguitar@gmail.com>
2024-05-17 10:41:08 +0900

    311b

---
+++ abc/311/b.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+// U, R, D, L
+vector<int> dy = {-1, 0, 1, 0};
+vector<int> dx = {0, 1, 0, -1};
+
+int main() {
+    ll N, D;
+    cin >> N >> D;
+    vector<string> S(N);
+    for (auto &s : S)
+        cin >> s;
+
+    ll count = 0;
+    ll ans = -1;
+
+    for (ll i = 0; i < D; i++) {
+        bool flag = true;
+        for (ll j = 0; j < N; j++) {
+            if (S[j][i] == 'x') {
+                ans = max(ans, count);
+                count = 0;
+                flag = false;
+            }
+        }
+
+        if (flag)
+            count++;
+    }
+
+    ans = max(ans, count);
+
+    cout << ans;
+}
