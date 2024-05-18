68b4ae476e087c5941a4603257fdae6c78c92ec0
otohusan <tamioguitar@gmail.com>
2024-05-18 17:49:48 +0900

    308cの少し修正

--- abc/308/c.cpp
+++ abc/308/c.cpp
@@ -9,16 +9,11 @@ vector<int> dx = {0, 1, 0, -1};
 int main() {
     ll N;
     cin >> N;
-    vector<ll> A(N);
-    vector<ll> B(N);
     map<ld, vector<ll>> M;
+
     for (int i = 0; i < N; i++) {
         ll a, b;
         cin >> a >> b;
-
-        A[i] = a;
-        B[i] = b;
-
         M[-(ld(a) / (a + b))].push_back(i);
     }
 
