908977fd99cfb276cbe9cf296ea1ae6050c7ba11
otohusan <tamioguitar@gmail.com>
2024-05-21 10:42:05 +0900

    306a

---
+++ abc/306/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+// U, R, D, L
+vector<int> dy = {-1, 0, 1, 0};
+vector<int> dx = {0, 1, 0, -1};
+
+int main() {
+    ll N;
+    string S;
+    cin >> N >> S;
+
+    for (ll i = 0; i < N; i++) {
+        cout << S[i] << S[i];
+    }
+}
