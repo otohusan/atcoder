492c47155aa579cb4038bea593004658f1a2cbaa
otohusan <tamioguitar@gmail.com>
2024-05-21 10:52:27 +0900

    306b

--- abc/306/b.cpp
+++ abc/306/b.cpp
@@ -1,8 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long int;
+using ll = unsigned long long int;
 // U, R, D, L
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll sum = 0;
+
+    for (ll i = 0; i < 64; i++) {
+        ll a;
+        cin >> a;
+
+        if (a == 1) {
+            ll kakeru = 1;
+            for (ll j = 0; j < i; j++) {
+                kakeru *= 2;
+            }
+
+            sum += kakeru;
+        }
+    }
+
+    cout << sum;
+}
