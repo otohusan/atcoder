802ebdef72d111cda9b19ce048a743a177d137ba
otohusan <tamioguitar@gmail.com>
2024-05-21 15:43:57 +0900

    304b

--- abc/304/b.cpp
+++ abc/304/b.cpp
@@ -5,4 +5,46 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N;
+    cin >> N;
+
+    if (N >= 100000000) {
+        N /= 1000000;
+        N *= 1000000;
+        cout << N;
+        return 0;
+    }
+    if (N >= 10000000) {
+        N /= 100000;
+        N *= 100000;
+        cout << N;
+        return 0;
+    }
+    if (N >= 1000000) {
+        N /= 10000;
+        N *= 10000;
+        cout << N;
+        return 0;
+    }
+    if (N >= 100000) {
+        N /= 1000;
+        N *= 1000;
+        cout << N;
+        return 0;
+    }
+    if (N >= 10000) {
+        N /= 100;
+        N *= 100;
+        cout << N;
+        return 0;
+    }
+    if (N >= 1000) {
+        N /= 10;
+        N *= 10;
+        cout << N;
+        return 0;
+    }
+
+    cout << N;
+}
