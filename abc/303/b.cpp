a0a4d86eddf4d70ba88c45e8d0ab8ecad89d3488
otohusan <tamioguitar@gmail.com>
2024-05-23 14:59:42 +0900

    303b

--- abc/303/b.cpp
+++ abc/303/b.cpp
@@ -5,4 +5,30 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, M;
+    cin >> N >> M;
+    vector<vector<ll>> G(M, vector<ll>(N));
+    for (ll i = 0; i < M; i++) {
+        for (ll j = 0; j < N; j++) {
+            cin >> G[i][j];
+        }
+    }
+
+    vector<set<ll>> ans(N + 1);
+
+    for (ll i = 0; i < M; i++) {
+        for (ll j = 0; j < N - 1; j++) {
+            ans[G[i][j]].insert(G[i][j + 1]);
+            ans[G[i][j + 1]].insert(G[i][j]);
+        }
+    }
+
+    ll sum = 0;
+
+    for (ll i = 1; i < N + 1; i++) {
+        sum += N - ans[i].size() - 1;
+    }
+
+    cout << sum / 2;
+}
