6cbbfadeb5302a849cd69e4e9443d5dac5dc0df6
otohusan <tamioguitar@gmail.com>
2024-05-25 09:51:11 +0900

    302c

--- abc/302/c.cpp
+++ abc/302/c.cpp
@@ -5,4 +5,35 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, M;
+    cin >> N >> M;
+    vector<string> S(N);
+    for (auto &s : S)
+        cin >> s;
+
+    sort(S.begin(), S.end());
+
+    do {
+        bool flag = true;
+        for (ll i = 0; i < N - 1; i++) {
+            ll cnt = 0;
+            for (ll k = 0; k < M; k++) {
+                if (S[i][k] != S[i + 1][k]) {
+                    cnt++;
+                }
+            }
+
+            if (cnt > 1)
+                flag = false;
+        }
+
+        if (flag) {
+            cout << "Yes";
+            return 0;
+        }
+
+    } while (next_permutation(S.begin(), S.end()));
+
+    cout << "No";
+}
