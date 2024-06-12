7b0cc238fafce76405e612d014a4e7a088c1cc1a
otohusan <tamioguitar@gmail.com>
2024-06-12 22:28:58 +0900

    301b

--- abc/301/b.cpp
+++ abc/301/b.cpp
@@ -5,4 +5,29 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N;
+    cin >> N;
+    vector<ll> A(N);
+    for (auto &a : A)
+        cin >> a;
+
+    cout << A[0] << " ";
+
+    for (ll i = 1; i < N; i++) {
+        if (abs(A[i - 1] - A[i]) < 1) {
+            cout << A[i] << " ";
+            continue;
+        }
+
+        if (A[i - 1] > A[i]) {
+            for (ll j = A[i - 1] - 1; j >= A[i]; j--) {
+                cout << j << " ";
+            }
+        } else {
+            for (ll j = A[i - 1] + 1; j <= A[i]; j++) {
+                cout << j << " ";
+            }
+        }
+    }
+}
