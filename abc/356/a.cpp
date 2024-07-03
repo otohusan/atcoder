1360babd0466075e97ace120fb56d4916be5b3e6
otohusan <tamioguitar@gmail.com>
2024-07-03 16:48:18 +0900

    356a

--- abc/356/a.cpp
+++ abc/356/a.cpp
@@ -5,4 +5,19 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, L, R;
+    cin >> N >> L >> R;
+
+    for (ll i = 1; i < L; i++) {
+        cout << i << " ";
+    }
+
+    for (ll i = R; i >= L; i--) {
+        cout << i << " ";
+    }
+
+    for (ll i = R + 1; i <= N; i++) {
+        cout << i << " ";
+    }
+}
