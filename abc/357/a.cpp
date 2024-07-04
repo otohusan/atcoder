9026b678fdafaae309defb5deedee80d0a09a509
otohusan <tamioguitar@gmail.com>
2024-07-04 13:15:35 +0900

    357a

--- abc/357/a.cpp
+++ abc/357/a.cpp
@@ -5,4 +5,23 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, M;
+    cin >> N >> M;
+
+    int ans = 0;
+
+    for (ll i = 0; i < N; i++) {
+        int H;
+        cin >> H;
+
+        if (M >= H) {
+            M -= H;
+            ans++;
+        } else {
+            break;
+        }
+    }
+
+    cout << ans;
+}
