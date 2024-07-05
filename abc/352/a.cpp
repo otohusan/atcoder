a848182be7b75e5db9eed51e279b7cea83ace8bb
otohusan <tamioguitar@gmail.com>
2024-07-05 10:04:01 +0900

    352a

--- abc/352/a.cpp
+++ abc/352/a.cpp
@@ -5,4 +5,12 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, X, Y, Z;
+    cin >> N >> X >> Y >> Z;
+
+    if (X > Y)
+        swap(X, Y);
+
+    cout << ((Z > X) && (Z < Y) ? "Yes" : "No");
+}
