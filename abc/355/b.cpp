26c1e173691ed128d53d79e77a89f78008db746d
otohusan <tamioguitar@gmail.com>
2024-07-05 21:35:15 +0900

    355b

--- abc/355/b.cpp
+++ abc/355/b.cpp
@@ -5,4 +5,31 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    set<int> A;
+    vector<int> C(N + M);
+
+    for (int i = 0; i < N + M; i++) {
+        int j;
+        cin >> j;
+
+        if (i < N)
+            A.insert(j);
+
+        C[i] = j;
+    }
+
+    sort(C.begin(), C.end());
+
+    for (int i = 0; i < N + M - 1; i++) {
+        if (A.count(C[i]) == true && A.count(C[i + 1]) == true) {
+            cout << "Yes";
+            return 0;
+        }
+    }
+
+    cout << "No";
+}
