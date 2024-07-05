0e71f86b49869b1ea8a1bf5867fc92ce4e326654
otohusan <tamioguitar@gmail.com>
2024-07-05 09:54:36 +0900

    359a

--- abc/359/a.cpp
+++ abc/359/a.cpp
@@ -5,4 +5,19 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+
+    int ans = 0;
+
+    for (int i = 0; i < N; i++) {
+        string S;
+        cin >> S;
+
+        if (S == "Takahashi")
+            ans++;
+    }
+
+    cout << ans;
+}
