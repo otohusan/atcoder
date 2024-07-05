f4c85dc022058ade7b0001b323e1d3acfe7f27f1
otohusan <tamioguitar@gmail.com>
2024-07-05 13:39:20 +0900

    354b

--- abc/354/b.cpp
+++ abc/354/b.cpp
@@ -5,4 +5,23 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+
+    ll sum = 0;
+    vector<string> S(N);
+
+    for (ll i = 0; i < N; i++) {
+        string s;
+        ll c;
+
+        cin >> s >> c;
+
+        S[i] = s;
+        sum += c;
+    }
+
+    sort(S.begin(), S.end());
+    cout << S[sum % N];
+}
