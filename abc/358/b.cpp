2da223b22c345ecadcf11f53908478ec5cefff78
otohusan <tamioguitar@gmail.com>
2024-07-08 19:50:07 +0900

    358b

--- abc/358/b.cpp
+++ abc/358/b.cpp
@@ -5,4 +5,21 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, A;
+    cin >> N >> A;
+
+    ll ans = 0;
+
+    for (ll i = 0; i < N; i++) {
+        ll t;
+        cin >> t;
+        if (ans < t) {
+            ans = t;
+        }
+
+        ans += A;
+
+        cout << ans << endl;
+    }
+}
