d5224d98a996ad1a0defd6f12cc5b2a5811a096a
otohusan <tamioguitar@gmail.com>
2024-07-08 17:27:46 +0900

    357b

--- abc/357/b.cpp
+++ abc/357/b.cpp
@@ -5,4 +5,28 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    string S;
+    cin >> S;
+
+    int upper = 0;
+    int lower = 0;
+
+    for (auto s : S) {
+        if (isupper(s)) {
+            upper++;
+        } else {
+            lower++;
+        }
+    }
+
+    if (upper > lower) {
+        for (auto &s : S)
+            s = toupper(s);
+    } else {
+        for (auto &s : S)
+            s = tolower(s);
+    }
+
+    cout << S;
+}
