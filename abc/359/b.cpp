b7a1d4c4125879e63b071e2ae2b5a5ff3aee6ddb
otohusan <tamioguitar@gmail.com>
2024-07-09 09:19:21 +0900

    359b

--- abc/359/b.cpp
+++ abc/359/b.cpp
@@ -5,4 +5,20 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> A(N + N);
+    for (auto &a : A)
+        cin >> a;
+
+    int ans = 0;
+
+    for (int i = 0; i < N + N - 2; i++) {
+        if (A[i] == A[i + 2])
+            ans++;
+    }
+
+    cout << ans;
+}
