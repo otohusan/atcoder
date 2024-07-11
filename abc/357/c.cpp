dbe73dc20f2049d38f088f8205429ae231fd5a1f
otohusan <tamioguitar@gmail.com>
2024-07-11 15:32:37 +0900

    357c

--- abc/357/c.cpp
+++ abc/357/c.cpp
@@ -1,8 +1,53 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long int;
-// U, R, D, L
-vector<int> dy = {-1, 0, 1, 0};
-vector<int> dx = {0, 1, 0, -1};
+#include <iostream>
+#include <vector>
 
-int main() {}
+using namespace std;
+
+void generate_carpet(vector<vector<char>> &carpet, int x, int y, int n) {
+    if (n == 0) {
+        carpet[x][y] = '#';
+    } else {
+        int size = 1;
+        for (int i = 0; i < n - 1; ++i) {
+            size *= 3;
+        }
+
+        // Create 3x3 blocks
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if (i == 1 && j == 1) {
+                    // Center block should be empty
+                    for (int dx = 0; dx < size; ++dx) {
+                        for (int dy = 0; dy < size; ++dy) {
+                            carpet[x + i * size + dx][y + j * size + dy] = '.';
+                        }
+                    }
+                } else {
+                    generate_carpet(carpet, x + i * size, y + j * size, n - 1);
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    int size = 1;
+    for (int i = 0; i < N; ++i) {
+        size *= 3;
+    }
+
+    vector<vector<char>> carpet(size, vector<char>(size, '.'));
+    generate_carpet(carpet, 0, 0, N);
+
+    for (const auto &row : carpet) {
+        for (const auto &cell : row) {
+            cout << cell;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
