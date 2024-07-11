4c7f8e6e3f97c113eecd5ab02a5229983d885cc7
otohusan <tamioguitar@gmail.com>
2024-07-11 12:13:29 +0900

    355c

--- abc/355/c.cpp
+++ abc/355/c.cpp
@@ -5,4 +5,71 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+bool sousa(ll retu, ll gyo, vector<vector<bool>> &G, ll N) {
+    bool flag = true;
+    for (ll i = 0; i < N; i++) {
+        if (G[i][retu] == false)
+            flag = false;
+    }
+    if (flag == true) {
+        return true;
+    }
+
+    flag = true;
+    for (ll i = 0; i < N; i++) {
+        if (G[gyo][i] == false)
+            flag = false;
+    }
+    if (flag == true) {
+        return true;
+    }
+
+    // 主対角線のチェック
+    if (gyo == retu) {
+        flag = true;
+        for (ll i = 0; i < N; i++) {
+            if (!G[i][i]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            return true;
+        }
+    }
+
+    flag = true;
+    for (ll i = 0; i < N; i++) {
+        if (G[i][N - 1 - i] == false)
+            flag = false;
+    }
+    if (flag == true) {
+        return true;
+    }
+
+    return false;
+}
+
+int main() {
+    ll N, T;
+    cin >> N >> T;
+
+    vector<vector<bool>> G(N, vector<bool>(N, false));
+
+    for (ll i = 0; i < T; i++) {
+        ll A;
+        cin >> A;
+
+        ll row = (A - 1) / N;
+        ll col = (A - 1) % N;
+
+        G[row][col] = true;
+
+        if (sousa(col, row, G, N)) {
+            cout << i + 1;
+            return 0;
+        }
+    }
+
+    cout << -1;
+}
