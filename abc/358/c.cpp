79cc1d4630299db1585f937894cd30420ba09355
otohusan <tamioguitar@gmail.com>
2024-07-11 18:21:24 +0900

    358c

--- abc/358/c.cpp
+++ abc/358/c.cpp
@@ -5,4 +5,41 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, M;
+    cin >> N >> M;
+
+    vector<string> S(N);
+    for (auto &s : S)
+        cin >> s;
+
+    int res = 1000000;
+
+    for (int bit = 0; bit < (1 << N); ++bit) {
+        vector<bool> ans(M, false);
+        int thisres = 0;
+
+        for (int i = 0; i < N; ++i) {
+            if (bit & (1 << i)) {
+                thisres++;
+
+                for (int j = 0; j < M; j++) {
+                    if (S[i][j] == 'o')
+                        ans[j] = true;
+                }
+            }
+        }
+
+        bool flag = true;
+
+        for (auto a : ans) {
+            if (!a)
+                flag = false;
+        }
+
+        if (flag)
+            res = min(res, thisres);
+    }
+
+    cout << res;
+}
