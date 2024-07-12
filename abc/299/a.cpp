3641c3ca530266aeea29beca5b337b912eb948ff
otohusan <tamioguitar@gmail.com>
2024-07-12 14:48:35 +0900

    299a

--- abc/299/a.cpp
+++ abc/299/a.cpp
@@ -5,4 +5,27 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    string S;
+    cin >> N >> S;
+
+    int count = 0;
+
+    for (auto s : S) {
+        if (s == '*') {
+            if (count == 1) {
+                cout << "in";
+            } else {
+                cout << "out";
+            }
+
+            return 0;
+        }
+
+        if (s == '|')
+            count++;
+    }
+
+    return 0;
+}
