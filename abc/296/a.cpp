8a1974150b50bc53574d0dfb167f4bed1ea6d708
otohusan <tamioguitar@gmail.com>
2024-07-13 09:58:02 +0900

    296a

--- abc/296/a.cpp
+++ abc/296/a.cpp
@@ -5,4 +5,17 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    string S;
+    cin >> N >> S;
+
+    for (int i = 0; i < N - 1; i++) {
+        if (S[i] == S[i + 1]) {
+            cout << "No";
+            return 0;
+        }
+    }
+
+    cout << "Yes";
+}
