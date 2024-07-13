e03d2a104116ae20d49982cb2c33aeea0ca73354
otohusan <tamioguitar@gmail.com>
2024-07-13 10:03:22 +0900

    295a

--- abc/295/a.cpp
+++ abc/295/a.cpp
@@ -5,4 +5,35 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        string w;
+        cin >> w;
+
+        if (w == "and") {
+            cout << "Yes";
+            return 0;
+        }
+        if (w == "not") {
+            cout << "Yes";
+            return 0;
+        }
+        if (w == "that") {
+            cout << "Yes";
+            return 0;
+        }
+        if (w == "the") {
+            cout << "Yes";
+            return 0;
+        }
+        if (w == "you") {
+            cout << "Yes";
+            return 0;
+        }
+    }
+
+    cout << "No";
+}
