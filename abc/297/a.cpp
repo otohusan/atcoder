daa676da0212f588117a6e13fe1551d20d415df5
otohusan <tamioguitar@gmail.com>
2024-07-13 09:55:35 +0900

    297a

--- abc/297/a.cpp
+++ abc/297/a.cpp
@@ -5,4 +5,24 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, D;
+    cin >> N >> D;
+
+    int p_v;
+    cin >> p_v;
+
+    for (int i = 0; i < N - 1; i++) {
+        int v;
+        cin >> v;
+
+        if (v - p_v <= D) {
+            cout << v;
+            return 0;
+        }
+
+        p_v = v;
+    }
+
+    cout << "-1";
+}
