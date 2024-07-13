f5d8d76746567af6a74e43e11ea426ef245b45d8
otohusan <tamioguitar@gmail.com>
2024-07-13 09:33:09 +0900

    298a

--- abc/298/a.cpp
+++ abc/298/a.cpp
@@ -5,4 +5,20 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    string S;
+    cin >> N >> S;
+
+    if (S.find('o') == string::npos) {
+        cout << "No";
+        return 0;
+    }
+
+    if (S.find('x') != string::npos) {
+        cout << "No";
+        return 0;
+    }
+
+    cout << "Yes";
+}
