fd6a2a285a3222ae6e00693d0578354a8de119d8
otohusan <tamioguitar@gmail.com>
2024-07-16 16:06:53 +0900

    296b

--- abc/296/b.cpp
+++ abc/296/b.cpp
@@ -5,4 +5,16 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    vector<string> S(8);
+    for (auto &s : S)
+        cin >> s;
+
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (S[i][j] == '*') {
+                cout << (char)('a' + j) << 8 - i;
+            }
+        }
+    }
+}
