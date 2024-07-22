45737b9de9bc1004298db2c644af010a5942171e
otohusan <tamioguitar@gmail.com>
2024-07-22 16:50:03 +0900

    299c

--- abc/299/c.cpp
+++ abc/299/c.cpp
@@ -5,4 +5,35 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    string S;
+    cin >> N >> S;
+
+    int ans = -1;
+
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        if (S[i] == 'o')
+            count++;
+
+        if (S[i] == '-' && count > 0) {
+            ans = max(ans, count);
+            count = 0;
+        }
+    }
+
+    count = 0;
+
+    for (int i = N - 1; i >= 0; i--) {
+        if (S[i] == 'o')
+            count++;
+
+        if (S[i] == '-' && count > 0) {
+            ans = max(ans, count);
+            count = 0;
+        }
+    }
+
+    cout << ans;
+}
