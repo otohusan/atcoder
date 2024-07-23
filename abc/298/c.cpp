ee381b8ddebf2f66fabfbc2b82a54520e644a03c
otohusan <tamioguitar@gmail.com>
2024-07-23 13:49:21 +0900

    298c

--- abc/298/c.cpp
+++ abc/298/c.cpp
@@ -5,4 +5,43 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, Q;
+    cin >> N >> Q;
+
+    map<ll, set<ll>> Card;
+    map<ll, priority_queue<ll, vector<ll>, greater<ll>>> Box;
+
+    for (int i = 0; i < Q; i++) {
+        int q;
+        cin >> q;
+
+        if (q == 1) {
+            int i, j;
+            cin >> i >> j;
+
+            Card[i].insert(j);
+            Box[j].push(i);
+        }
+
+        if (q == 2) {
+            int i;
+            cin >> i;
+            priority_queue<ll, vector<ll>, greater<ll>> q = Box[i];
+            while (!q.empty()) {
+                cout << q.top() << " ";
+                q.pop();
+            }
+            cout << endl;
+        }
+
+        if (q == 3) {
+            int i;
+            cin >> i;
+            for (auto s : Card[i]) {
+                cout << s << " ";
+            }
+            cout << endl;
+        }
+    }
+}
