2512cddeb8506d1663408ef6411795b3becf737f
otohusan <tamioguitar@gmail.com>
2024-07-24 08:41:24 +0900

    294b

--- abc/294/b.cpp
+++ abc/294/b.cpp
@@ -5,4 +5,21 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int H, W;
+    cin >> H >> W;
+    vector<vector<int>> S(H, vector<int>(W));
+
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            cin >> S[i][j];
+        }
+    }
+
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            cout << (S[i][j] == 0 ? '.' : (char)('A' + S[i][j] - 1));
+        }
+        cout << endl;
+    }
+}
