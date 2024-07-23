72e75ccf83f0bef847f74ab37cabf62483308676
otohusan <tamioguitar@gmail.com>
2024-07-23 18:08:29 +0900

    292a

--- abc/292/a.cpp
+++ abc/292/a.cpp
@@ -5,4 +5,9 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    string S;
+    cin >> S;
+    for (auto &s : S)
+        cout << (char)toupper(s);
+}
