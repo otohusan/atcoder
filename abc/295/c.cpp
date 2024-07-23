2282160a9fadcfe3a71476bdf02a240d45c7ae95
otohusan <tamioguitar@gmail.com>
2024-07-23 12:53:43 +0900

    295c

--- abc/295/c.cpp
+++ abc/295/c.cpp
@@ -5,4 +5,22 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N;
+    cin >> N;
+    map<ll, ll> M;
+
+    for (ll i = 0; i < N; i++) {
+        ll a;
+        cin >> a;
+
+        M[a]++;
+    }
+
+    ll ans = 0;
+    for (auto m : M) {
+        ans += m.second / 2;
+    }
+
+    cout << ans;
+}
