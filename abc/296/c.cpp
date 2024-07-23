373a974b45ade8b8613af4e4dd239671fb2bf029
otohusan <tamioguitar@gmail.com>
2024-07-23 12:46:46 +0900

    296c

--- abc/296/c.cpp
+++ abc/296/c.cpp
@@ -5,4 +5,27 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, X;
+    cin >> N >> X;
+
+    map<ll, bool> M;
+
+    vector<ll> A(N);
+    for (auto &a : A)
+        cin >> a;
+
+    for (ll i = 0; i < N; i++) {
+        M[A[i]] = true;
+    }
+
+    for (ll i = 0; i < N; i++) {
+
+        if (M[X + A[i]]) {
+            cout << "Yes";
+            return 0;
+        }
+    }
+
+    cout << "No";
+}
