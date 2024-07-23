18e3bf5a1a26e2b3330d861837eb2083d184dd50
otohusan <tamioguitar@gmail.com>
2024-07-23 17:59:21 +0900

    294a

--- abc/294/a.cpp
+++ abc/294/a.cpp
@@ -5,4 +5,15 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+
+        if (a % 2 == 0)
+            cout << a << " ";
+    }
+}
