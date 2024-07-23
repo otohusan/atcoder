dcd49287514e657a8223855ec040d00071134f33
otohusan <tamioguitar@gmail.com>
2024-07-23 09:27:50 +0900

    297c

--- abc/297/c.cpp
+++ abc/297/c.cpp
@@ -5,4 +5,26 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll H, W;
+    cin >> H >> W;
+    vector<string> S(H);
+    for (auto &s : S)
+        cin >> s;
+
+    for (ll i = 0; i < H; i++) {
+        for (ll j = 0; j < W - 1; j++) {
+            if (S[i][j] == 'T' && S[i][j + 1] == 'T') {
+                S[i][j] = 'P';
+                S[i][j + 1] = 'C';
+            }
+        }
+    }
+
+    for (ll i = 0; i < H; i++) {
+        for (ll j = 0; j < W; j++) {
+            cout << S[i][j];
+        }
+        cout << endl;
+    }
+}
