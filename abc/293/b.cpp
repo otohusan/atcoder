6fd543f1b2a5b4fbf6b24a590664fb081ecbe4a6
otohusan <tamioguitar@gmail.com>
2024-07-24 09:12:19 +0900

    293b

--- abc/293/b.cpp
+++ abc/293/b.cpp
@@ -5,4 +5,32 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    vector<bool> ans(N, false);
+    for (auto &a : A)
+        cin >> a;
+
+    for (int i = 0; i < N; i++) {
+        if (!ans[i]) {
+            ans[A[i] - 1] = true;
+        }
+    }
+
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        if (!ans[i])
+            count++;
+    }
+
+    cout << count << endl;
+
+    for (int i = 0; i < N; i++) {
+        if (ans[i])
+            continue;
+
+        cout << i + 1 << " ";
+    }
+}
