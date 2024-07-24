06e9ffc70d36ef02949c96fd9725aa8e49fbc73e
otohusan <tamioguitar@gmail.com>
2024-07-24 14:58:30 +0900

    290a

--- abc/290/a.cpp
+++ abc/290/a.cpp
@@ -5,4 +5,19 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N), B(M);
+    for (auto &a : A)
+        cin >> a;
+    for (auto &a : B)
+        cin >> a;
+
+    int ans = 0;
+    for (auto b : B) {
+        ans += A[b - 1];
+    }
+
+    cout << ans;
+}
