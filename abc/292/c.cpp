5b187b7f37451e48c18ac4ea7f9f5397a724b2cc
otohusan <tamioguitar@gmail.com>
2024-07-24 13:34:24 +0900

    293c

--- abc/292/c.cpp
+++ abc/292/c.cpp
@@ -1,8 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long int;
-// U, R, D, L
-vector<int> dy = {-1, 0, 1, 0};
-vector<int> dx = {0, 1, 0, -1};
+using ll = long long;
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+
+    // A * B の値をカウントするためのmap
+    unordered_map<int, ll> AB_count;
+
+    // A, B をすべての組み合わせを考慮
+    for (int A = 1; A <= N; ++A) {
+        for (int B = 1; B <= N / A; ++B) {
+            AB_count[A * B]++;
+        }
+    }
+
+    ll count = 0;
+
+    // AB_countの値を使ってCDを探す
+    for (const auto &[AB, numAB] : AB_count) {
+        if (AB_count[N - AB]) {
+            count += numAB * AB_count[N - AB];
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
