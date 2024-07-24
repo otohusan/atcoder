e8ac0ad8d58014b67a1c97f3557f433b770dc43a
otohusan <tamioguitar@gmail.com>
2024-07-24 09:58:54 +0900

    292b

--- abc/292/b.cpp
+++ abc/292/b.cpp
@@ -5,4 +5,27 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+
+    vector<int> ans(N, 0);
+
+    for (int i = 0; i < Q; i++) {
+        int q, x;
+        cin >> q >> x;
+        x--;
+
+        if (q == 1) {
+            ans[x]++;
+        }
+
+        if (q == 2) {
+            ans[x] += 2;
+        }
+
+        if (q == 3) {
+            cout << (ans[x] >= 2 ? "Yes" : "No") << endl;
+        }
+    }
+}
