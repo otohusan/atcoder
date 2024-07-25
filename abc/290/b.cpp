1b96b4f2b71fa37e144caabe00d161f24b70fdcf
otohusan <tamioguitar@gmail.com>
2024-07-25 18:48:36 +0900

    290b

--- abc/290/b.cpp
+++ abc/290/b.cpp
@@ -5,4 +5,19 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, K;
+    cin >> N >> K;
+
+    for (ll i = 0; i < N; i++) {
+        char s;
+        cin >> s;
+
+        if (s == 'o' && K > 0) {
+            cout << 'o';
+            K--;
+        } else {
+            cout << 'x';
+        }
+        }
+}
