44b17e36ba9ce191da36dc7d3ee8c7f0f3dbbc94
otohusan <tamioguitar@gmail.com>
2024-07-25 18:38:10 +0900

    291b

--- abc/291/b.cpp
+++ abc/291/b.cpp
@@ -5,4 +5,20 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+    vector<ll> A(N * 5);
+    for (auto &a : A)
+        cin >> a;
+
+    sort(A.begin(), A.end());
+
+    ll ans = 0;
+
+    for (ll i = N; i < 4 * N; i++) {
+        ans += A[i];
+    }
+
+    cout << ans / (long double)(N * 3);
+}
