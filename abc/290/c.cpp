a6174b421a3f36ebd9794e1d0804efece394f4f0
otohusan <tamioguitar@gmail.com>
2024-07-26 10:27:01 +0900

    290c

--- abc/290/c.cpp
+++ abc/290/c.cpp
@@ -5,4 +5,22 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, K;
+    cin >> N >> K;
+    map<ll, bool> A;
+    for (int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+        A[a] = true;
+    }
+
+    int ans = K;
+
+    for (int i = 0; i < K; i++) {
+        if (A[i] == false)
+            ans = min(ans, i);
+    }
+
+    cout << ans;
+}
