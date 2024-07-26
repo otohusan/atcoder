47e09b01daa8a5ce3b5d761e75574dd367d834e1
otohusan <tamioguitar@gmail.com>
2024-07-26 09:54:05 +0900

    291c

--- abc/291/c.cpp
+++ abc/291/c.cpp
@@ -5,4 +5,31 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
+    int x, y = 0;
+    set<pair<int, int>> P = {{x, y}};
+
+    for (int i = 0; i < N; i++) {
+        if (S[i] == 'U')
+            y++;
+        if (S[i] == 'R')
+            x++;
+        if (S[i] == 'D')
+            y--;
+        if (S[i] == 'L')
+            x--;
+
+        if (P.count({x, y})) {
+            cout << "Yes";
+            return 0;
+        }
+
+        P.insert({x, y});
+    }
+
+    cout << "No";
+}
