160f092fae68ad9b62679304bf3f4b2808d80031
otohusan <tamioguitar@gmail.com>
2024-07-26 11:20:34 +0900

    289b

--- abc/289/b.cpp
+++ abc/289/b.cpp
@@ -5,4 +5,30 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, M;
+    cin >> N >> M;
+    queue<int> Q;
+    for (int i = 0; i < M; i++) {
+        int q;
+        cin >> q;
+        Q.push(q);
+    }
+
+    stack<int> S;
+
+    for (int i = 0; i < N; i++) {
+        if (Q.front() == i + 1) {
+            S.push(Q.front());
+            Q.pop();
+            continue;
+        }
+
+        cout << i + 1 << " ";
+
+        while (!S.empty()) {
+            cout << S.top() << " ";
+            S.pop();
+        }
+    }
+}
