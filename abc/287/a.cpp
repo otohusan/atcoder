0a94c7cec3d43e94b7468b4d2114ad0041a9a4bd
otohusan <tamioguitar@gmail.com>
2024-07-26 10:51:31 +0900

    287a

--- abc/287/a.cpp
+++ abc/287/a.cpp
@@ -5,4 +5,21 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+    int count = 0;
+
+    for (int i = 0; i < N; i++) {
+        string s;
+        cin >> s;
+
+        if (s == "For") {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    cout << (count >= 0 ? "Yes" : "No");
+}
