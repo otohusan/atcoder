9f1a5d3a15d45087e0a08768b5dce5a897b4de5e
otohusan <tamioguitar@gmail.com>
2024-07-26 11:32:35 +0900

    288b

--- abc/288/b.cpp
+++ abc/288/b.cpp
@@ -5,4 +5,16 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<string> S(M);
+    for (auto &n : S)
+        cin >> n;
+
+    sort(S.begin(), S.end());
+
+    for (int i = 0; i < M; i++) {
+        cout << S[i] << endl;
+    }
+}
