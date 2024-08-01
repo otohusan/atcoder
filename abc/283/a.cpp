9587224b6d0979a542ac3e4cb3a8aa5c53cccc68
otohusan <tamioguitar@gmail.com>
2024-08-01 14:45:11 +0900

    283a

--- abc/283/a.cpp
+++ abc/283/a.cpp
@@ -5,4 +5,15 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    ll ans = 1;
+
+    for (int i = 0; i < B; i++) {
+        ans *= A;
+    }
+
+    cout << ans;
+}
