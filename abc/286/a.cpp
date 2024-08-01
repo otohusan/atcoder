f3522ac08172a64095ee36e8f12eba58b4f8cb51
otohusan <tamioguitar@gmail.com>
2024-08-01 12:12:59 +0900

    286a

--- abc/286/a.cpp
+++ abc/286/a.cpp
@@ -5,4 +5,18 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, P, Q, R, S;
+    cin >> N >> P >> Q >> R >> S;
+    vector<int> A(N);
+    for (auto &a : A)
+        cin >> a;
+
+    for (int i = 0; i <= Q - P; i++) {
+        swap(A[i + P - 1], A[i + R - 1]);
+    }
+
+    for (auto a : A) {
+        cout << a << " ";
+    }
+}
