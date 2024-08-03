6d5a2adf6553a384a383febdb0c01f44086ac296
otohusan <tamioguitar@gmail.com>
2024-08-03 14:49:55 +0900

    285b

--- abc/285/b.cpp
+++ abc/285/b.cpp
@@ -5,4 +5,22 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    string S;
+    cin >> N >> S;
+
+    for (int i = 1; i < N; i++) {
+        int ans = 0;
+
+        for (int j = 0; j < N - i; j++) {
+            if (S[j] == S[j + i]) {
+                break;
+            }
+
+            ans++;
+        }
+
+        cout << ans << endl;
+    }
+}
