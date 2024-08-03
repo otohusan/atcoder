20c8ae6c35a4d67f917c33296b7bd7a7b5fb90a5
otohusan <tamioguitar@gmail.com>
2024-08-03 14:55:30 +0900

    284b

--- abc/284/b.cpp
+++ abc/284/b.cpp
@@ -5,4 +5,24 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        int n;
+        cin >> n;
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int a;
+            cin >> a;
+
+            if (a % 2 == 1)
+                ans++;
+        }
+
+        cout << ans << endl;
+    }
+}
