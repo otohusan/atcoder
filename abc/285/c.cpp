94c7c480c74464bb34237edfaca6c35a79a0513a
otohusan <tamioguitar@gmail.com>
2024-08-03 18:32:08 +0900

    285c

--- abc/285/c.cpp
+++ abc/285/c.cpp
@@ -5,4 +5,25 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    string S;
+    cin >> S;
+
+    ll ans = 0;
+
+    reverse(S.begin(), S.end());
+
+    int keta = 0;
+    for (auto s : S) {
+        ll keta_sum = 1;
+        for (int i = 0; i < keta; i++) {
+            keta_sum *= 26;
+        }
+
+        ans += keta_sum * ((int)s - 'A' + 1);
+
+        keta++;
+    }
+
+    cout << ans;
+}
