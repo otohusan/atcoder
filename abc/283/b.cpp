c9fe0644509e8d5ceaca42c38a93a8c7947a52bc
otohusan <tamioguitar@gmail.com>
2024-08-03 15:04:19 +0900

    283b

--- abc/283/b.cpp
+++ abc/283/b.cpp
@@ -5,4 +5,30 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (auto &a : A)
+        cin >> a;
+
+    int Q;
+    cin >> Q;
+
+    for (int i = 0; i < Q; i++) {
+        int q;
+        cin >> q;
+
+        if (q == 1) {
+            int k, x;
+            cin >> k >> x;
+
+            A[k - 1] = x;
+        } else {
+            int k;
+            cin >> k;
+
+            cout << A[k - 1] << endl;
+        }
+    }
+}
