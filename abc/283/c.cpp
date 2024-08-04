8603aa1e518de335cd06ad5b73a07c9ba4f488db
otohusan <tamioguitar@gmail.com>
2024-08-04 16:44:54 +0900

    283c

--- abc/283/c.cpp
+++ abc/283/c.cpp
@@ -1,8 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long int;
+using ll = unsigned long int;
 // U, R, D, L
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    string S;
+    cin >> S;
+
+    ll ans = 0;
+    for (ll i = 0; i < S.size(); i++) {
+        if (i + 1 < S.size() && S[i] == '0' && S[i + 1] == '0') {
+            i++;
+            ans++;
+            continue;
+        }
+
+        ans++;
+    }
+
+    cout << ans;
+}
