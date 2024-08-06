5043cb25ddeb26fa52b92e4338312c89e70661d8
otohusan <tamioguitar@gmail.com>
2024-08-06 10:33:23 +0900

    280a

--- abc/280/a.cpp
+++ abc/280/a.cpp
@@ -5,4 +5,20 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, W;
+    cin >> N >> W;
+    vector<string> G(N);
+    for (auto &g : G)
+        cin >> g;
+
+    int ans = 0;
+
+    for (int i = 0; i < N; i++) {
+        for (auto g : G[i])
+            if (g == '#')
+                ans++;
+    }
+
+    cout << ans;
+}
