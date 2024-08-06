b12a46f7e873865cd6749cc562d522417cc21d5b
otohusan <tamioguitar@gmail.com>
2024-08-06 10:26:38 +0900

    282a

--- abc/282/a.cpp
+++ abc/282/a.cpp
@@ -5,4 +5,11 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int K;
+    cin >> K;
+
+    for (int i = 0; i < K; i++) {
+        cout << (char)('A' + i);
+    }
+}
