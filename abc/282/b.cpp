a02da6c6b6d25199ef484ceaa79afe17504b1b20
otohusan <tamioguitar@gmail.com>
2024-08-09 13:42:30 +0900

    282b

--- abc/282/b.cpp
+++ abc/282/b.cpp
@@ -5,4 +5,26 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<string> S(N);
+    for (auto &s : S)
+        cin >> s;
+
+    int ans = 0;
+
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = i + 1; j < N; j++) {
+            for (int x = 0; x < M; x++) {
+                if (S[i][x] == 'x' && S[j][x] == 'x') {
+                    ans--;
+                    break;
+                }
+            }
+            ans++;
+        }
+    }
+
+    cout << ans;
+}
