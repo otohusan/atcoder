6bfa2b079e2f3928296202ab9db95a12c478a1b4
otohusan <tamioguitar@gmail.com>
2024-08-14 12:42:05 +0900

    280b

--- abc/280/b.cpp
+++ abc/280/b.cpp
@@ -5,4 +5,17 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N;
+    cin >> N;
+
+    ll sum = 0;
+
+    for (ll i = 0; i < N; i++) {
+        ll s;
+        cin >> s;
+
+        cout << s - sum << " ";
+        sum += s - sum;
+    }
+}
