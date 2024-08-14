f7e84147e2ab7166b19f69771465459214507ee0
otohusan <tamioguitar@gmail.com>
2024-08-14 12:26:28 +0900

    281b

--- abc/281/b.cpp
+++ abc/281/b.cpp
@@ -5,4 +5,36 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    string S;
+    cin >> S;
+
+    if (S.size() != 8) {
+        cout << "No";
+        return 0;
+    }
+
+    if (!isupper(S[0]) || !isupper(S[S.size() - 1])) {
+        cout << "No";
+        return 0;
+    }
+
+    string di;
+
+    for (int i = 1; i < S.size() - 1; i++) {
+        if (!isdigit(S[i])) {
+            cout << "No";
+            return 0;
+        }
+
+        di += S[i];
+    }
+
+    int num = stoi(di);
+    if (num < 100000 || num > 999999) {
+        cout << "No" << endl;
+        return 0;
+    }
+
+    cout << "Yes";
+}
