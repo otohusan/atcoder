0ee848206d9cb0096d360e8f6dcf2d15c1939c08
otohusan <tamioguitar@gmail.com>
2024-08-15 12:51:15 +0900

    362a

--- abc/362/a.cpp
+++ abc/362/a.cpp
@@ -5,4 +5,20 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int r, g, b;
+    cin >> r >> g >> b;
+    string s;
+    cin >> s;
+
+    int ans = 100;
+
+    if (s != "Red" && r < ans)
+        ans = r;
+    if (s != "Green" && g < ans)
+        ans = g;
+    if (s != "Blue" && b < ans)
+        ans = b;
+
+    cout << ans;
+}
