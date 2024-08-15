a8d0370fcff1dae41fbeae0d86429d056726517a
otohusan <tamioguitar@gmail.com>
2024-08-15 09:39:10 +0900

    280c

--- abc/280/c.cpp
+++ abc/280/c.cpp
@@ -5,4 +5,14 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    string S, T;
+    cin >> S >> T;
+
+    for (int i = 0; i < T.size(); i++) {
+        if (S[i] != T[i]) {
+            cout << i + 1;
+            break;
+        }
+    }
+}
