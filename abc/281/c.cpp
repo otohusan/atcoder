b06e5ca0ff96f30a9779f4b908a3f4f0748e1c0e
otohusan <tamioguitar@gmail.com>
2024-08-15 09:30:23 +0900

    281c

--- abc/281/c.cpp
+++ abc/281/c.cpp
@@ -5,4 +5,25 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, T;
+    cin >> N >> T;
+    vector<ll> A(N);
+    ll sum = 0;
+    for (auto &a : A) {
+        cin >> a;
+        sum += a;
+    }
+
+    T %= sum;
+
+    for (ll i = 0; i < N; i++) {
+
+        if (T - A[i] < 0) {
+            cout << i + 1 << " " << T;
+            break;
+        }
+
+        T -= A[i];
+    }
+}
