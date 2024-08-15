4d5939f489d4cb73e33aa535976cbb89bf9bf619
otohusan <tamioguitar@gmail.com>
2024-08-15 09:51:35 +0900

    360a

--- abc/360/a.cpp
+++ abc/360/a.cpp
@@ -5,4 +5,21 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    string S;
+    cin >> S;
+
+    int r_count = 0;
+
+    for (int i = 0; i < S.size(); i++) {
+        if (S[i] == 'R')
+            r_count++;
+
+        if (S[i] == 'M' && r_count) {
+            cout << "Yes";
+            return 0;
+        }
+    }
+
+    cout << "No";
+}
