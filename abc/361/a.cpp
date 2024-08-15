ffeb62a5bf01c83119588b72f6a3d6589ed7080a
otohusan <tamioguitar@gmail.com>
2024-08-15 12:42:10 +0900

    361a

--- abc/361/a.cpp
+++ abc/361/a.cpp
@@ -5,4 +5,16 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int n, k, x;
+    cin >> n >> k >> x;
+
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+
+        cout << a << " ";
+        if (i + 1 == k)
+            cout << x << " ";
+    }
+}
