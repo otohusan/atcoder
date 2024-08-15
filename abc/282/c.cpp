7100227c2550b3de759b971c1021de058c578287
otohusan <tamioguitar@gmail.com>
2024-08-15 09:01:08 +0900

    282c

--- abc/282/c.cpp
+++ abc/282/c.cpp
@@ -5,4 +5,24 @@ using ll = long long int;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    string S;
+    cin >> N >> S;
+
+    bool toggle = false;
+
+    for (int i = 0; i < N; i++) {
+        if (S[i] == '"') {
+            toggle = !toggle;
+            continue;
+        }
+
+        if (S[i] == ',' && !toggle) {
+            S[i] = '.';
+            continue;
+        }
+    }
+
+    cout << S;
+}
