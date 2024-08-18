18d1e479c858982692f49be03f62727a3b190562
otohusan <tamioguitar@gmail.com>
2024-08-18 21:55:20 +0900

    360b

--- abc/360/b.cpp
+++ abc/360/b.cpp
@@ -1,8 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long int;
-// U, R, D, L
-vector<int> dy = {-1, 0, 1, 0};
-vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    string s, t;
+    cin >> s >> t;
+    for (int w = 1; w < (int)s.size(); ++w) {
+        for (int c = 0; c < w; ++c) {
+            string now = "";
+            for (int i = c; i < (int)s.size(); i += w) {
+                now += s[i];
+            }
+            if (now == t) {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No" << endl;
+}
