a20b47c5fab65b43ba37db8360bbe466c8677021
otohusan <tamioguitar@gmail.com>
2024-08-20 11:15:28 +0900

    362b

--- abc/362/b.cpp
+++ abc/362/b.cpp
@@ -1,29 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long int;
-// U, R, D, L
-vector<int> dy = {-1, 0, 1, 0};
-vector<int> dx = {0, 1, 0, -1};
+
+// 2点間の距離の2乗を計算する関数
+ll squared_distance(pair<int, int> p1, pair<int, int> p2) {
+    ll dx = p1.first - p2.first;
+    ll dy = p1.second - p2.second;
+    return dx * dx + dy * dy;
+}
 
 int main() {
-    vector<pair<int, int>> T(3);
-    for (int i = 0; i < 3; i++) {
-        int a, b;
-        cin >> a >> b;
+    vector<pair<int, int>> points(3);
 
-        T[i] = {a, b};
+    // 3点の入力を受け取る
+    for (int i = 0; i < 3; i++) {
+        cin >> points[i].first >> points[i].second;
     }
 
-    int A = ((T[0].first - T[1].first) * (T[0].first - T[1].first)) +
-            ((T[0].second - T[1].second) * (T[0].second - T[1].second));
-    int B = ((T[0].first - T[2].first) * (T[0].first - T[2].first)) +
-            ((T[0].second - T[2].second) * (T[0].second - T[2].second));
-    int C = ((T[1].first - T[2].first) * (T[1].first - T[2].first)) +
-            ((T[1].second - T[2].second) * (T[1].second - T[2].second));
+    // 3辺の長さの2乗を計算
+    ll A = squared_distance(points[0], points[1]);
+    ll B = squared_distance(points[0], points[2]);
+    ll C = squared_distance(points[1], points[2]);
 
+    // ピタゴラスの定理による直角三角形の判定
     if (A == B + C || B == A + C || C == A + B) {
         cout << "Yes" << endl;
     } else {
         cout << "No" << endl;
     }
+
+    return 0;
 }
