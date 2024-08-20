da85878bdfcb4ef37a8e1f15601b265eafcf24b8
otohusan <tamioguitar@gmail.com>
2024-08-20 21:38:01 +0900

    363b

--- abc/363/b.cpp
+++ abc/363/b.cpp
@@ -6,4 +6,15 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, T, P;
+    cin >> N >> T >> P;
+
+    vector<int> A(N);
+    for (auto &a : A)
+        cin >> a;
+
+    sort(A.rbegin(), A.rend());
+
+    cout << (T - A[P - 1] > 0 ? T - A[P - 1] : 0);
+}
