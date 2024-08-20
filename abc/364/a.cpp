dcbccf497807f34866cd45d1162da5fe8cd7b4f5
otohusan <tamioguitar@gmail.com>
2024-08-20 21:28:33 +0900

    364a

--- abc/364/a.cpp
+++ abc/364/a.cpp
@@ -6,4 +6,23 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+
+    string be = "";
+
+    for (int i = 0; i < N - 1; i++) {
+        string s;
+        cin >> s;
+
+        if (s == "sweet" && be == s) {
+            cout << "No";
+            return 0;
+        }
+
+        be = s;
+    }
+
+    cout << "Yes";
+}
