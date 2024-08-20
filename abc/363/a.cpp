e2ab288b9ae68b76a4570a7b00dbdc11a3675455
otohusan <tamioguitar@gmail.com>
2024-08-20 21:22:49 +0900

    363a

--- abc/363/a.cpp
+++ abc/363/a.cpp
@@ -6,4 +6,22 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int R;
+    cin >> R;
+
+    if (R < 100) {
+        cout << 100 - R;
+        return 0;
+    }
+
+    if (R < 200) {
+        cout << 200 - R;
+        return 0;
+    }
+
+    if (R < 300) {
+        cout << 300 - R;
+        return 0;
+    }
+}
