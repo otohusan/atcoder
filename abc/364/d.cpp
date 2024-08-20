84ad9cfbf39b7ce173d669bdd4a2174f29c478f8
otohusan <tamioguitar@gmail.com>
2024-08-20 21:08:11 +0900

    ファイル作成

---
+++ abc/364/d.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+const ll INF = LLONG_MAX;
+// U, R, D, L
+vector<int> dy = {-1, 0, 1, 0};
+vector<int> dx = {0, 1, 0, -1};
+
+int main() {}
