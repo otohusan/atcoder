3ef467e0ebbc7722b396a1414eba10e3fbbdc15b
otohusan <tamioguitar@gmail.com>
2024-08-21 14:10:48 +0900

    365a

--- abc/365/a.cpp
+++ abc/365/a.cpp
@@ -6,4 +6,13 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int Y;
+    cin >> Y;
+
+    if (Y % 4 != 0 || (Y % 100 == 0 && Y % 400 != 0)) {
+        cout << "365";
+    } else {
+        cout << "366";
+    }
+}
