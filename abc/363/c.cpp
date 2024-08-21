e1951acbaad710a4b9b5ac9bd39cfb8a1790422a
otohusan <tamioguitar@gmail.com>
2024-08-21 13:37:03 +0900

    363c

--- abc/363/c.cpp
+++ abc/363/c.cpp
@@ -6,4 +6,28 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, K;
+    string S;
+    cin >> N >> K >> S;
+
+    sort(S.begin(), S.end());
+    ll ans = 0;
+
+    do {
+        ans++;
+
+        for (int i = 0; i < N - (K - 1); i++) {
+            string r_S = S.substr(i, K);
+            reverse(r_S.begin(), r_S.end());
+
+            if (r_S == S.substr(i, K)) {
+                ans--;
+                break;
+            }
+        }
+
+    } while (next_permutation(S.begin(), S.end()));
+
+    cout << ans;
+}
