272d8b10dcdf999afc079208bc8e9467ab771b51
otohusan <tamioguitar@gmail.com>
2024-08-21 13:58:14 +0900

    364c

--- abc/364/c.cpp
+++ abc/364/c.cpp
@@ -6,4 +6,37 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N, X, Y;
+    cin >> N >> X >> Y;
+    vector<ll> A(N), B(N);
+    for (auto &a : A)
+        cin >> a;
+    for (auto &a : B)
+        cin >> a;
+
+    sort(A.begin(), A.end(), greater<ll>());
+    sort(B.begin(), B.end(), greater<ll>());
+
+    ll ans = N;
+
+    for (ll i = 0; i < N; i++) {
+        if (X < 0) {
+            ans = min(ans, i);
+            break;
+        }
+
+        X -= A[i];
+    }
+
+    for (ll i = 0; i < N; i++) {
+        if (Y < 0) {
+            ans = min(ans, i);
+            break;
+        }
+
+        Y -= B[i];
+    }
+
+    cout << ans;
+}
