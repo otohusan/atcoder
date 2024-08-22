b220d5305902a0c6e00465457470805a506fab3e
otohusan <tamioguitar@gmail.com>
2024-08-22 09:58:33 +0900

    365b

--- abc/365/b.cpp
+++ abc/365/b.cpp
@@ -6,4 +6,18 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    ll N;
+    cin >> N;
+    vector<pair<ll, ll>> A(N);
+    for (int i = 0; i < N; i++) {
+        ll a;
+        cin >> a;
+
+        A[i] = {a, i};
+    }
+
+    sort(A.begin(), A.end());
+
+    cout << A[N - 2].second + 1;
+}
