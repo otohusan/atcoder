829b130724ca4917895f167019f870ccca5a63f7
otohusan <tamioguitar@gmail.com>
2024-08-24 11:52:05 +0900

    366b

--- abc/366/b.cpp
+++ abc/366/b.cpp
@@ -1,9 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long int;
-const ll INF = LLONG_MAX;
-// U, R, D, L
-vector<int> dy = {-1, 0, 1, 0};
-vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+    vector<string> S(N);
+    for (auto &s : S)
+        cin >> s;
+    int M = 0;
+    for (auto &s : S)
+        M = max(M, (int)s.size());
+    vector<string> T(M, string(N, '*'));
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < (int)S[i].size(); ++j) {
+            T[j][N - i - 1] = S[i][j];
+        }
+    }
+    for (int i = 0; i < M; ++i) {
+        while (T[i].back() == '*') {
+            T[i].pop_back();
+        }
+        cout << T[i] << endl;
+    }
+}
