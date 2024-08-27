56d28e861623c55343cb348b93e90da19bd301e6
otohusan <tamioguitar@gmail.com>
2024-08-27 17:00:52 +0900

    367a

--- abc/367/a.cpp
+++ abc/367/a.cpp
@@ -6,4 +6,18 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    for (int i = 0; i < 24; i++) {
+        if ((b + i) % 24 == c)
+            break;
+        if ((b + i) % 24 == a) {
+            cout << "No";
+            return 0;
+        }
+    }
+
+    cout << "Yes";
+}
