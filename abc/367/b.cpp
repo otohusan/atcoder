26b7dc0a3c31e9e9d435d208dd964192ea079e25
otohusan <tamioguitar@gmail.com>
2024-08-27 17:27:26 +0900

    367b

--- abc/367/b.cpp
+++ abc/367/b.cpp
@@ -6,4 +6,23 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    string s;
+    cin >> s;
+
+    string ans = s;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[s.length() - 1 - i] == '.') {
+            ans.pop_back();
+            break;
+        }
+        if (s[s.length() - 1 - i] == '0') {
+            ans.pop_back();
+            continue;
+        }
+        break;
+    }
+
+    cout << ans;
+}
