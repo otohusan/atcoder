3543f510f6271b65d16b9616b4b451984033f2b4
otohusan <tamioguitar@gmail.com>
2024-08-29 20:52:14 +0900

    367c入力だけ

--- abc/367/c.cpp
+++ abc/367/c.cpp
@@ -6,4 +6,10 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() { cout << "go"; }
+int main() {
+    int N, K;
+    cin >> N >> K;
+    vector<int> R(N);
+    for (auto &r : R)
+        cin >> r;
+}
