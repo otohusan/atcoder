2c6469e5a5b21b74717d273c79114746bdbbba23
otohusan <tamioguitar@gmail.com>
2024-08-30 13:17:24 +0900

    367c

--- abc/367/c.cpp
+++ abc/367/c.cpp
@@ -1,10 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long int;
-const ll INF = LLONG_MAX;
-// U, R, D, L
-vector<int> dy = {-1, 0, 1, 0};
-vector<int> dx = {0, 1, 0, -1};
+
+void generateCombinations(int N, int K, const vector<int> &R,
+                          vector<int> &combination, int index, int sum) {
+    if (index == N) {
+        if (sum % K == 0) {
+            for (int num : combination) {
+                cout << num << " ";
+            }
+            cout << endl;
+        }
+        return;
+    }
+
+    for (int i = 1; i <= 5; i++) {
+        if (i <= R[index]) {
+            combination[index] = i;
+            generateCombinations(N, K, R, combination, index + 1, sum + i);
+        }
+    }
+}
 
 int main() {
     int N, K;
@@ -12,4 +28,9 @@ int main() {
     vector<int> R(N);
     for (auto &r : R)
         cin >> r;
+
+    vector<int> combination(N);
+    generateCombinations(N, K, R, combination, 0, 0);
+
+    return 0;
 }
