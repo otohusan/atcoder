123faf5a25140e3e1821377ab5bd7236e73bdaf8
otohusan <tamioguitar@gmail.com>
2024-08-30 13:33:15 +0900

    368a

--- abc/368/a.cpp
+++ abc/368/a.cpp
@@ -6,4 +6,18 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N, K;
+    cin >> N >> K;
+    vector<int> A(N);
+    for (auto &a : A)
+        cin >> a;
+
+    for (int i = N - K; i < N; i++) {
+        cout << A[i] << " ";
+    }
+
+    for (int i = 0; i < N - K; i++) {
+        cout << A[i] << " ";
+    }
+}
