44c1bb3bc22bd941ea2760d60f90449528872b39
otohusan <tamioguitar@gmail.com>
2024-08-30 15:46:46 +0900

    368c

--- abc/368/c.cpp
+++ abc/368/c.cpp
@@ -6,4 +6,34 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+    vector<ll> H(N);
+    for (auto &h : H)
+        cin >> h;
+
+    ll ans = 0;
+
+    for (int i = 0; i < N; i++) {
+        ans += (H[i] / 5) * 3;
+        ll amari = H[i] % 5;
+        H[i] = amari;
+
+        for (int j = 0; j < amari; j++) {
+            ans++;
+
+            if (ans % 3 == 0) {
+                H[i] -= 3;
+            } else {
+                H[i] -= 1;
+            }
+
+            if (H[i] <= 0) {
+                break;
+            }
+        }
+    }
+
+    cout << ans;
+}
