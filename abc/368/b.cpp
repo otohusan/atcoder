aa9ea7ea3da12d3c86edbfc8f5f35e89181e01d5
otohusan <tamioguitar@gmail.com>
2024-08-30 13:48:57 +0900

    368b

--- abc/368/b.cpp
+++ abc/368/b.cpp
@@ -6,4 +6,29 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (auto &a : A)
+        cin >> a;
+
+    int ans = 0;
+
+    while (1) {
+        int count = 0;
+        for (auto a : A) {
+            if (a > 0)
+                count++;
+        }
+        if (count <= 1) {
+            cout << ans;
+            return 0;
+        }
+
+        sort(A.rbegin(), A.rend());
+        A[0]--;
+        A[1]--;
+        ans++;
+    }
+}
