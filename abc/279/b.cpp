c1084855ec2fe0901a7d44319ec34e4bb1a3484d
otohusan <tamioguitar@gmail.com>
2024-09-03 12:32:38 +0900

    279b

--- abc/279/b.cpp
+++ abc/279/b.cpp
@@ -6,4 +6,21 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    string S, T;
+    cin >> S >> T;
+
+    if (S.length() < T.length()) {
+        cout << "No";
+        return 0;
+    }
+
+    for (int i = 0; i < S.length() - T.length() + 1; i++) {
+        if (T == S.substr(i, T.length())) {
+            cout << "Yes";
+            return 0;
+        }
+    }
+
+    cout << "No";
+}
