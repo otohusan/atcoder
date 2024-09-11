18c2258ebf22763c6d20159056fe6462d5c1b676
otohusan <tamioguitar@gmail.com>
2024-09-11 20:11:44 +0900

    369a

--- abc/369/a.cpp
+++ abc/369/a.cpp
@@ -6,4 +6,18 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if (a == b) {
+        cout << 1;
+        return 0;
+    }
+
+    if ((a + b) % 2 == 0) {
+        cout << 3;
+    } else {
+        cout << 2;
+    }
+}
