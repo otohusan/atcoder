33452ee177e75c3c5ad489e3661ffe2faa04b6d8
otohusan <tamioguitar@gmail.com>
2024-09-24 19:57:20 +0900

    369b

--- abc/369/b.cpp
+++ abc/369/b.cpp
@@ -6,4 +6,42 @@ const ll INF = LLONG_MAX;
 vector<int> dy = {-1, 0, 1, 0};
 vector<int> dx = {0, 1, 0, -1};
 
-int main() {}
+int N;
+vector<int> A;
+vector<char> S;
+
+ll tryy(int start_left, int start_right) {
+    int res = 0;
+
+    for (int i = 0; i < N; i++) {
+        if (S[i] == 'L') {
+            res += abs(A[i] - start_left);
+            start_left = A[i];
+        }
+        if (S[i] == 'R') {
+            res += abs(A[i] - start_right);
+            start_right = A[i];
+        }
+    }
+
+    return (ll)res;
+}
+
+int main() {
+    cin >> N;
+    A.resize(N);
+    S.resize(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i] >> S[i];
+    }
+
+    ll ans = INF;
+
+    for (int i = 1; i <= 100; i++) {
+        for (int j = 1; j <= 100; j++) {
+            ans = min(ans, tryy(i, j));
+        }
+    }
+
+    cout << ans;
+}
