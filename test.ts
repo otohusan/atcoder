29bdbb16eff07eb9ff6a7677b2377a6273428593
otohusan <tamioguitar@gmail.com>
2025-01-30 22:24:22 +0900

    ts

--- test.ts
+++ test.ts
@@ -1,4 +1,14 @@
 function majorityElement(nums: number[]): number {
-  nums.sort((a, b) => a - b);
-  return nums[Math.floor(nums.length / 2)];
+  const map = new Map<number, number>();
+  const majorityCount = Math.floor(nums.length / 2);
+
+  for (let num of nums) {
+    map.set(num, (map.get(num) ?? 0) + 1);
+
+    if (map.get(num)! > majorityCount) {
+      return num;
+    }
+  }
+
+  return -1; // ここには到達しない（問題の条件より、必ず存在する）
 }
