5df1a1c70af5522c350fc228f63defa93ceb3c00
otohusan <tamioguitar@gmail.com>
2025-02-01 12:56:14 +0900

    ts

--- leetcode/55. Jump Game.ts
+++ leetcode/55. Jump Game.ts
@@ -1,18 +1,10 @@
 function canJump(nums: number[]): boolean {
-  const ans: boolean[] = Array(nums.length).fill(false);
-  ans[0] = true;
+  let maxReach = 0;
 
-  for (let index = 0; index < nums.length; index++) {
-    if (ans[index] === true) {
-      for (let j = 1; j <= nums[index]; j++) {
-        if (nums.length < index + j) {
-          break;
-        }
-
-        ans[index + j] = true;
-      }
-    }
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxReach) return false;
+    maxReach = Math.max(maxReach, i + nums[i]);
   }
 
-  return ans[ans.length - 1];
+  return true;
 }
