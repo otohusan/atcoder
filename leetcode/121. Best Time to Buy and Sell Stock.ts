eeadfba1f6730eeca2dfeee5543914357150a03f
otohusan <tamioguitar@gmail.com>
2025-02-01 10:47:53 +0900

    ts

--- leetcode/121. Best Time to Buy and Sell Stock.ts
+++ leetcode/121. Best Time to Buy and Sell Stock.ts
@@ -1,11 +1,11 @@
 function maxProfit(prices: number[]): number {
-  let ans = 0;
+  let minPrice = Infinity;
+  let maxProfit = 0;
 
-  for (let i = 0; i < prices.length; i++) {
-    for (let j = i + 1; j < prices.length; j++) {
-      ans = Math.max(ans, prices[j] - prices[i]);
-    }
+  for (let price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
   }
 
-  return ans;
+  return maxProfit;
 }
