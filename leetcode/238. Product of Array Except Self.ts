d18daee5f50110d1704909fb5897b9765d3fec79
otohusan <tamioguitar@gmail.com>
2025-02-05 12:56:30 +0900

    ts

--- leetcode/238. Product of Array Except Self.ts
+++ leetcode/238. Product of Array Except Self.ts
@@ -1,1 +1,27 @@
-function productExceptSelf(nums: number[]): number[] {}
+function productExceptSelf(nums: number[]): number[] {
+  let zero_count = 0;
+  let sum = 1;
+
+  for (let index = 0; index < nums.length; index++) {
+    if (nums[index] === 0) {
+      zero_count++;
+      continue;
+    }
+
+    sum *= nums[index];
+  }
+
+  if (zero_count >= 2) {
+    return Array(nums.length).fill(0);
+  }
+
+  const ans: number[] = Array(nums.length).fill(sum);
+
+  for (let index = 0; index < nums.length; index++) {
+    if (nums[index] === 0) continue;
+    if (zero_count > 0) ans[index] = 0;
+    ans[index] = ans[index] / nums[index];
+  }
+
+  return ans;
+}
