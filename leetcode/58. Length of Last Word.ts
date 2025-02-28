976fe3de57aa84131bd81fc79740d94eace95abb
otohusan <tamioguitar@gmail.com>
2025-02-28 15:32:03 +0900

    58

---
+++ leetcode/58. Length of Last Word.ts
@@ -0,0 +1,15 @@
+function lengthOfLastWord(s: string): number {
+  let ans = 0;
+
+  for (let index = 0; index < s.length; index++) {
+    if (s[s.length - index - 1] !== " ") {
+      ans++;
+    }
+
+    if (s[s.length - index - 1] === " " && ans > 0) {
+      return ans;
+    }
+  }
+
+  return ans;
+}
