3acea80ba8866b2d1b736c3308277d261daf9c3e
otohusan <tamioguitar@gmail.com>
2025-03-02 20:58:43 +0900

    28

--- leetcode/28. Find the Index of the First Occurrence in a String.ts
+++ leetcode/28. Find the Index of the First Occurrence in a String.ts
@@ -1,19 +1,12 @@
 function strStr(haystack: string, needle: string): number {
-  if (haystack === needle) return 0;
+  if (needle === "") return 0;
 
-  let ans = -1;
-  for (let index = 0; index < haystack.length; index++) {
-    if (index + needle.length > haystack.length) {
-      break;
-    }
-
+  for (let index = 0; index <= haystack.length - needle.length; index++) {
     const a = haystack.slice(index, index + needle.length);
-
     if (a === needle) {
-      ans = index;
-      break;
+      return index;
     }
   }
 
-  return ans;
+  return -1;
 }
